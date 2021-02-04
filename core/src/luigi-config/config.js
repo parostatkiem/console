cc239208fe1001076b87804a708fd5f8fca13c39
dariadomagala <daria.domagala@sap.com>
2021-02-04 15:35:02 +0100

    Use config map if present

--- core/src/luigi-config/config.js
+++ core/src/luigi-config/config.js
@@ -1,23 +1,32 @@
-import { getApiUrl } from './auth/auth-params'
+// import { getApiUrl } from './auth/auth-params' - for the future
 
-const k8sDomain = getApiUrl() || 'kyma.local';
-const localDomain = 'console-dev.' + k8sDomain;
+const clusterConfig = window['clusterConfig'] || INJECTED_CLUSTER_CONFIG;
+const domain = clusterConfig?.domain || 'kyma.local';
+const localDomain = 'console-dev.' + domain;
 const isLocalDev = window.location.href.startsWith(
   `http://${localDomain}:4200`
 );
 
 export const config = {
-  domain: k8sDomain,
-  localDomain: 'console-dev.' + k8sDomain,
-  serviceCatalogModuleUrl: isLocalDev ? 'http://console-dev.'+ k8sDomain + ':8000' : 'https://catalog.' + k8sDomain,
-  addOnsModuleUrl: isLocalDev ? 'http://console-dev.'+ k8sDomain + ':8004' : 'https://addons.' + k8sDomain,
-  logsModuleUrl: isLocalDev ? 'http://console-dev.'+ k8sDomain + ':8005' : 'https://logs.' + k8sDomain,
-  coreModuleUrl: isLocalDev ? 'http://console-dev.'+ k8sDomain +':8889' : 'https://core-ui.' + k8sDomain,
-  graphqlApiUrl: 'https://console-backend.' + k8sDomain + '/graphql',
-  apiserverUrl: 'https://apiserver.' + k8sDomain,
+  domain,
+  localDomain,
+  serviceCatalogModuleUrl: isLocalDev ? 'http://console-dev.'+ domain + ':8000' : 'https://catalog.' + domain,
+  addOnsModuleUrl: isLocalDev ? 'http://console-dev.'+ domain + ':8004' : 'https://addons.' + domain,
+  logsModuleUrl: isLocalDev ? 'http://console-dev.'+ domain + ':8005' : 'https://logs.' + domain,
+  coreModuleUrl: isLocalDev ? 'http://console-dev.'+ domain +':8889' : 'https://core-ui.' + domain,
+  pamelaApiUrl: 'https://pamela.' + domain,
+  graphqlApiUrl: 'https://console-backend.' + domain + '/graphql',
+  apiserverUrl: 'https://apiserver.' + domain,
   disabledNavigationNodes: '',
   namespaceAdminGroupName: 'runtimeNamespaceAdmin',
   runtimeAdminGroupName: 'runtimeAdmin',
   systemNamespaces:
     'compass-system istio-system knative-eventing knative-serving kube-public kube-system kyma-backup kyma-installer kyma-integration kyma-system natss kube-node-lease kubernetes-dashboard serverless-system',
+  headerLogoUrl: 'assets/logo.svg',
+  headerTitle: '',
+  faviconUrl: 'favicon.ico',
+  defaultIdpJwksUri: 'http://dex-service.kyma-system.svc.cluster.local:5556/keys',
+  defaultIdpIssuer: 'https://dex.' + domain,
+  bebEnabled: false,
+  ...clusterConfig
 };
--- core/src/luigi-config/settings.js
+++ core/src/luigi-config/settings.js
@@ -1,29 +1,14 @@
 import { config } from './config';
 
-const createHeader = () => {
-  // const logo =
-  //   config && config.headerLogoUrl
-  //     ? config.headerLogoUrl
-  //     : '/assets/logo.svg';
-  // const title = config?.headerTitle || null;
-  
-  // const favicon = config ? config.faviconUrl : undefined;
-  const logo = '/assets/logo.svg';
-  const title = null;
-  
-  const favicon = 'favicon.ico';
-  return {
-    logo,
-    title,
-    favicon
-  };
-};
-
 export const settings = 
 {
   responsiveNavigation: 'simpleMobileOnly',
   sideNavFooterText: '',
-  header: createHeader(), 
+  header: {
+    logo: config.headerLogoUrl,
+    title: config.headerTitle,
+    favicon: config.faviconUrl,
+  }, 
   appLoadingIndicator: {
     hideAutomatically: false
   },
