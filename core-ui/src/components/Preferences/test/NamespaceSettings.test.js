897771e68f795c2cd3e100ff227614583998b57a
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-02-17 09:22:36 +0100

    Add testcase

--- core-ui/src/components/Preferences/test/NamespaceSettings.test.js
+++ core-ui/src/components/Preferences/test/NamespaceSettings.test.js
@@ -3,12 +3,22 @@ import LuigiClient from '@luigi-project/client';
 import { render, fireEvent } from '@testing-library/react';
 import NamespaceSettings from '../NamespaceSettings';
 
+let mockGroups = [];
 jest.mock('react-shared', () => ({
-  useMicrofrontendContext: () => ({ showSystemNamespaces: true }),
+  useMicrofrontendContext: () => ({
+    showSystemNamespaces: true,
+    groups: mockGroups,
+  }),
 }));
 
 describe('NamespaceSettings', () => {
+  it('Renders nothing if groups are an array missing runtimeAdmin', () => {
+    const { queryByText } = render(<NamespaceSettings />);
+    expect(queryByText('Namespace Settings')).not.toBeInTheDocument();
+  });
+
   it('Sends custom message on toggle', () => {
+    mockGroups = null;
     const spy = jest.spyOn(LuigiClient, 'sendCustomMessage');
     const { getByLabelText } = render(<NamespaceSettings />);
 
