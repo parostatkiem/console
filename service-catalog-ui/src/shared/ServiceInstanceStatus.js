5c5be42e6b0315ab42beb01e7b714169ffe9658c
parostatkiem <jan.sudczak@sap.com>
2021-03-08 14:35:59 +0100

    improve status column

--- service-catalog-ui/src/shared/ServiceInstanceStatus.js
+++ service-catalog-ui/src/shared/ServiceInstanceStatus.js
@@ -4,16 +4,17 @@ import { StatusBadge } from 'react-shared';
 import { getBadgeTypeForStatus } from 'helpers/getBadgeTypeForStatus';
 
 export const ServiceInstanceStatus = ({ instance }) => {
-  const type = instance.status.conditions?.length
-    ? instance.status.conditions[0].type
-    : 'UNKNOWN';
+  const lastCondition =
+    instance.status.conditions?.length && instance.status.conditions[0];
+
+  if (!lastCondition) return <StatusBadge type="info">UNKNOWN</StatusBadge>;
 
   return (
     <StatusBadge
-      tooltipContent={instance.status.conditions[0]?.message}
-      type={getBadgeTypeForStatus(type.toUpperCase())}
+      tooltipContent={lastCondition.message}
+      type={getBadgeTypeForStatus(lastCondition.reason.toUpperCase())}
     >
-      {type}
+      {lastCondition.reason}
     </StatusBadge>
   );
 };
