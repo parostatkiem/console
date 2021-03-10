71af7b9976feade3834d9cc544e63709d8aab1ea
parostatkiem <jan.sudczak@sap.com>
2021-03-10 14:28:26 +0100

    add possibility to calculate or manually enter the eventType data

--- core-ui/src/shared/components/EventTriggers/CreateEventTriggerForm.js
+++ core-ui/src/shared/components/EventTriggers/CreateEventTriggerForm.js
@@ -1,15 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Checkbox from 'components/Lambdas/Checkbox/Checkbox';
-import { ComboboxInput, Menu, FormInput } from 'fundamental-react';
-
+// import { ComboboxInput, Menu, FormInput } from 'fundamental-react';
+import {
+  FormItem,
+  FormLabel,
+  FormInput,
+  FormGroup,
+  FormFieldset,
+  FormLegend,
+} from 'fundamental-react';
 import { SchemaComponent } from './Schema/Schema';
-
+import { StringInput } from 'react-shared';
 import './CreateEventTriggerForm.scss';
 
 export default function CreateEventTriggerForm({
   formElementRef,
-
   isLambda = false,
   setCustomValid = () => void 0,
   onSubmit,
@@ -23,8 +29,31 @@ export default function CreateEventTriggerForm({
   //   // setCustomValid(!!events.filter(e => e.isChecked).length);
   // }, [events, setCustomValid]);
 
+  const [calculatedEventType, setCalculatedEventType] = useState(null);
+
+  const eventAppInput = useRef(),
+    eventTypeInput = useRef(),
+    eventVersionInput = useRef();
+
+  const inputFields = [eventAppInput, eventTypeInput, eventVersionInput];
+
+  const eventTypeFinalInput = useRef();
+
   async function handleSubmit() {
     // await onSubmit(events.filter(e => e.isChecked));
+  }
+
+  async function calculateEventType() {
+    if (!inputFields.every(i => i.current?.value)) return; //cannot calculate value
+
+    const newValue = inputFields.map(f => f.current.value).join('.');
+    eventTypeFinalInput.current.value = newValue;
+  }
+
+  async function handleEventTypeManualChange(e) {
+    inputFields.forEach(i => {
+      if (i.current) i.current.value = '';
+    });
   }
 
   return (
@@ -34,7 +63,76 @@ export default function CreateEventTriggerForm({
       onChange={onChange}
       className="create-event-trigger-form"
     >
-      <input type="text" />
+      <FormFieldset>
+        <h2 className="fd-has-type-3">Calculate eventType field value</h2>
+
+        <FormItem>
+          <FormLabel
+            htmlFor="event_app"
+            required
+            className="fd-has-display-block"
+          >
+            Application name
+          </FormLabel>
+          <FormInput
+            ref={eventAppInput}
+            onChange={calculateEventType}
+            placeholder="Name of the application which is the source of the event"
+            id="event_app"
+          />
+        </FormItem>
+        <FormItem>
+          <FormLabel
+            htmlFor="event_type"
+            required
+            className="fd-has-display-block"
+          >
+            Event type
+          </FormLabel>
+          <FormInput
+            onChange={calculateEventType}
+            ref={eventTypeInput}
+            placeholder="Type of the event"
+            id="event_type"
+          />
+        </FormItem>
+        <FormItem>
+          <FormLabel
+            htmlFor="event_version"
+            required
+            className="fd-has-display-block"
+          >
+            Event version
+          </FormLabel>
+          <FormInput
+            onChange={calculateEventType}
+            ref={eventVersionInput}
+            placeholder="Version of the event"
+            id="event_version"
+            defaultValue="v1"
+          />
+        </FormItem>
+      </FormFieldset>
+      <div className="create-event-trigger-form--divider"></div>
+      <FormFieldset>
+        <h2 className="fd-has-type-3">Enter eventType field value manually</h2>
+        <FormItem>
+          <FormLabel
+            htmlFor="final_value"
+            required
+            className="fd-has-display-block"
+          >
+            eventType field value
+          </FormLabel>
+          <FormInput
+            ref={eventTypeFinalInput}
+            onChange={handleEventTypeManualChange}
+            required
+            placeholder="The eventType value used to create the subscriptions"
+            id="final_value"
+          />
+        </FormItem>
+      </FormFieldset>
     </form>
   );
 }
